20ee5d145d44aaae21892f24714c3384384277f2
VaheNersis <v.nersisyan2000@mail.ru>
2023-12-05 17:31:20 +0400

    second

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
+        title: Text('BM1I CALCULATOR'),
       ),
       body:Center(
         child: Text('Body Text'),
